ce139b101893bcb90f4ed7bb63479edb30b8b724
alsmd <dangerveneta@gmail.com>
2021-08-28 15:05:17 -0300

    changing fireball's range

--- src/actions/fireball_action.c
+++ src/actions/fireball_action.c
@@ -6,7 +6,7 @@
 /*   By: flavio <flavio@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/08/27 18:56:44 by flavio            #+#    #+#             */
-/*   Updated: 2021/08/28 12:20:33 by flavio           ###   ########.fr       */
+/*   Updated: 2021/08/28 15:04:54 by flavio           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -43,6 +43,7 @@ void	draw_fireball(t_fireball *fireball, t_game *game)
 	y = (fireball->y - game->map.y) * BLOCK_SIZE;
 	copy_img_to(&game->map.map, &fireball->sprite,
 		to_array(x, y, BLOCK_SIZE, BLOCK_SIZE));
+	check_player_collision(fireball->x, fireball->y, game);
 }
 
 void	render_fireball(t_game *game)
@@ -56,7 +57,7 @@ void	render_fireball(t_game *game)
 	{
 		if (is_on_map_fireball(fireball, game))
 			draw_fireball(fireball, game);
-		if (fireball->steps == 10)
+		if (fireball->steps == 8)
 		{
 			free_fireball(last_one, fireball, game);
 			fireball = last_one;
@@ -68,7 +69,6 @@ void	render_fireball(t_game *game)
 			fireball->x += 1;
 			fireball->steps += 1;
 		}
-		check_player_collision(fireball->x, fireball->y, game);
 		last_one = fireball;
 		fireball = fireball->next;
 	}
